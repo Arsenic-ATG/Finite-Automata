2032cbf63fb0010e03667c98c211789e8647abb8
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-02-10 17:00:50 +0530

    Docs: remove useless doc files
    remove and prevent html and latex files to be generated by doxygen as
    sphinx only needs xml files

--- docs/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['finite_5fautomata_0',['finite_automata',['../classfa_1_1finite__automata.html',1,'fa']]]
-];
--- docs/html/search/functions_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['move_0',['move',['../classfa_1_1finite__automata.html#ac9abcd3ecbd7934796de66ea3be42a78',1,'fa::finite_automata::move(state current_state, symbol scanned_symbol) const -&gt; std::set&lt; state &gt;'],['../classfa_1_1finite__automata.html#acbc50ad41da15371bf3d747007452916',1,'fa::finite_automata::move(std::set&lt; state &gt; states, symbol scanned_symbol) const -&gt; std::set&lt; state &gt;']]]
-];
